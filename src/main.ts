b12dda90f946f66f41a783b87c60844019b7a971
Behruz <behruzbahtiyorov10@gmail.com>
2025-07-11 14:42:21 +0500

    main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Telegraf } from 'telegraf';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const bot = app.get<Telegraf>(getBotToken());
+  app.use(bot.webhookCallback('/'));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
